9b47fb307fd9a13373badbb3d591da4c83969958
Pao Perez <pao.pacheco.perez@gmail.com>
2023-08-23 00:25:06 -0600

    Implement error component

---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Error.css';
+
+export default function Error() {
+    return (
+        <section>
+            <p>Encountered an error while processing your request...</p>
+        </section>
+    );
+}
